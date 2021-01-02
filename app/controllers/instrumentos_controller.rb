55da5a755bb0bb4c95c30924f111a60b7e8c36b5
jesus <jelopezgonzalez@gmail.com>
2021-01-02 17:41:24 +0000

    avances crud

--- app/controllers/instrumentos_controller.rb
+++ app/controllers/instrumentos_controller.rb
@@ -5,6 +5,29 @@ class InstrumentosController < ApplicationController
   end
 
   def create
-
+    @instrumento = Instrumento.create(nombre: params[:instrumento][:nombre])
+    render json: @instrumento
   end
+
+  def show
+    @instrumento = Instrumento.find(params[:id])
+  end
+
+  def edit
+    @instrumento = Instrumento.find(params[:id])
+  end
+
+  def update
+    @instrumento = Instrumento.find(params[:id])
+    @instrumento.update(nombre: params[:instrumento][:nombre])
+    #render json: @instrumento
+    redirect_to @instrumento
+  end
+
+  def destroy
+    @instrumento = Instrumento.find(params[:id])
+    @instrumento.destroy
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: "instrumentos#index"
+
   get "instrumentos/new", to: "instrumentos#new"
   post "instrumentos", to: "instrumentos#create"
+  get "instrumentos/:id", to: "instrumentos#show"
+  get "instrumentos/:id/edit", to: "instrumentos#edit"
+  patch "instrumentos/:id", to: "instrumentos#update", as: :instrumento
+  delete "instrumentos/:id", to: "instrumentos#destroy"
 
 end
