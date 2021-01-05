59f326f6739a017c98c86a559cf4d0b046ef4248
jesus <jelopezgonzalez@gmail.com>
2021-01-05 10:32:11 +0000

    validar hecho

--- app/models/instrumento.rb
+++ app/models/instrumento.rb
@@ -1,3 +1,6 @@
 class Instrumento < ApplicationRecord
   validates :nombre, presence: true, length: {minimum: 5, maximum: 50}
+  validates :tipo, presence: true, length: {minimum: 3, maximum: 15}
+  validates :precio, presence: true, length: {minimum: 1, maximum: 5}
+  validates :detalles, presence: true, length: {minimum: 1, maximum: 150}
 end
