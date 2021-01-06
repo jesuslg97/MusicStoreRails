da6fe4760356c5824575477469f5d86d57abf238
jesus <jelopezgonzalez@gmail.com>
2021-01-06 19:40:30 +0000

    botones form

--- app/controllers/instrumentos_controller.rb
+++ app/controllers/instrumentos_controller.rb
@@ -52,7 +52,7 @@ class InstrumentosController < ApplicationController
 
     def strong_params_instrumento
       params.require(:instrumento).permit(:nombre, :tipo, :detalles, :precio,
-                                          :image, :idCategoria, :color, :material, :id)
+                                          :image, :idCategoria, :color, :material)
     end
 
 end
