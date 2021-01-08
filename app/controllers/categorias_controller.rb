af27b821015411813081f836aaaed72b8ff4978e
Sara <saraalmonacid98@gmail.com>
2021-01-08 21:33:38 +0000

    Vistas y navegación de index, leer y actualizar

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -9,81 +9,38 @@ class CategoriasController < ApplicationController
   end
 
   def leer
-    @categorias = params[:url]
-    @categorias = Categorium.where(url: @categorias)
+    @categoria = Categorium.find(params[:id])
   end
 
   def crear
-    @categorias = Categorium.new
+    @categoria = Categorium.new
   end
 
   def actualizar
-    @categorias = Categorium.find(params[:id])
-    @categorias = Categorium.where(id: @categorias)
-  end
-  # def editar
-  #
-  #   # Pasamos el 'id' del registro a actualizar (método editar)
-  #   @categorias = Categorium.find(params[:id])
-  #
-  #   # Actualizamos el Archivo al servidor
-  #   uploaded_file = params[:img]
-  #
-  #   if params[:img].present?
-  #
-  #     # Eliminamos el archivo (imagen) anterior
-  #     simg = Categorium.where(:id => @categorias).pluck(:img)
-  #     imgeliminar = Rails.root.join('public', 'assets/img', simg.join)
-  #     File.delete(Rails.root + imgeliminar)
-  #
-  #     # Subimos el nuevo archivo (imagen)
-  #     File.open(Rails.root.join('public', 'assets/img', uploaded_file.original_filename), 'wb') do |file|
-  #       file.write(uploaded_file.read)
-  #     end
-  #
-  #   else
-  #     #
-  #   end
-  #
-  #   # Actualizamos un determinado registro en la base de datos
-  #   if @categorias.update(parametros)
-  #
-  #     # Actualizamos la columna 'img' de la base de datos
-  #     if params[:img].present?
-  #       @categorias.update_column(:img, uploaded_file.original_filename)
-  #     else
-  #       #
-  #     end
-  #
-  #   else
-  #     render :edit
-  #   end
-  #
-  #   # Redireccionamos a la vista principal con mensaje
-  #   @ini = "/categorias/index"
-  #   flash[:notice] = "Actualizado Correctamente !"
-  #   redirect_to @ini
-  #
-  # end
-
-  # Procesamos la eliminación de un registro de la base de datos
-  def eliminar
-    @categorias = Categorium.find(params[:id])
-
-    # # Eliminamos la imagen perteneciente al registro
-    # simg = Categorium.where(:id => @categorias).pluck(:img)
-    # imgeliminar = Rails.root.join('public', 'assets/img', simg.join)
-    # File.delete(Rails.root + imgeliminar)
-    #
-    # Categorium.where(id: @categorias ).destroy_all
-    # Redireccionamos a la vista principal con mensaje
-    #     @ini = "/categorias/index"
-    #     flash[:notice] = "Eliminado Correctamente !"
-    #     redirect_to @ini
+    @categoria = Categorium.find(params[:id])
   end
 
+  def editar
+    logger.debug "holssaSAAAAAAAAAAAAAA"
+
+    @categoria = Categorium.find(params[:id])
+
+    #TODO: guardado de imagen
+
+    if @categoria.update(parametros)
+      # Redireccionamos a la vista principal con mensaje
+      flash[:notice] = "Actualizado Correctamente !"
+      redirect_to "/categorias/index"
+    else
+      render :edit
+    end
+
+  end
+
+  # Parámetros o campos que insertamos o actualizamos en la base de datos
   private
-  def parametros
-    params.permit(:nombre, :descripcion, :img)
-  end
+    def parametros
+      params.permit()
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'categorias/leer/:id', to: 'categorias#leer'
   get 'categorias/crear'
   get 'categorias/actualizar/:id', to: 'categorias#actualizar'
+  post 'categorias/editar/:id', to: 'categorias#editar'
   get 'categorias/eliminar/:id', to: 'categorias#eliminar'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
