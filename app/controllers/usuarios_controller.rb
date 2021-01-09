508476a39cbba1dd943ae33166f22f8d2fb85d2a
Álvaro <alvarosn_98@hotmail.com>
2021-01-09 01:36:59 +0000

    New y Show

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,6 +1,6 @@
 class UsuariosController < ApplicationController
 
-  @root_url = "/usuarios/index"
+  #@root_url = "/usuarios/index"
   layout 'application'
   add_flash_types :notice
 
@@ -8,25 +8,39 @@ class UsuariosController < ApplicationController
     @usuario = Usuario.all()
   end
 
-  def show
-
-  end
-
   def new
     @usuario = Usuario.new
   end
 
   def create
-    @usuario = Usuario.create(strong_params_instrumento)
+    @usuario = Usuario.create(params_usuarios)
     render json: @usuario
   end
 
-  def update
+  def show
+    @usuario = Usuario.find(params[:id])
+  end
 
+  def edit
+  end
+
+  def update
+    if @instrumento.update(strong_params_instrumento)
+      redirect_to @instrumento
+    else
+      render 'edit'
+    end
   end
 
   def delete
 
   end
+
+  private
+    def params_usuarios
+      params.require(:usuario).permit(:nombre, :apellidos, :pais, :localidad,
+                                      :direccion, :codigoPostal, :email, :contraseña)
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get 'usuarios/index', to "usuarios#index"
-  get 'usuarios/show', to "usuarios#show"
-  get 'usuarios/new', to: "usuarios#new"
+
+  root to: "usuarios#index"
+
+  get 'usuarios', to: 'usuarios#index'
+  get 'usuarios/new', to: 'usuarios#new', as: :new_usuario
   post "usuarios", to: "usuarios#create"
-  get 'usuarios/update', to "usuarios#update"
+  get 'usuarios/:id', to: "usuarios#show"
+  get 'usuarios/:id/edit', to: "usuarios#edit"
+  patch "usuarios/:id", to: "usuarios#update"
+  delete "usuarios/:id", to: "usuarios#delete"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
